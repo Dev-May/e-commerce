0e7d898873674a27b66653300f41c7b0e05a301c
Dev-May <mai.eldhrawy@gmail.com>
2025-02-09 20:15:30 +0200

    cart feature

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -20,9 +20,51 @@ function addToCart(productId) {
     .catch((err) => err);
 }
 
+function getLoggedUserCart() {
+  return axios
+    .get("https://ecommerce.routemisr.com/api/v1/cart", { headers })
+    .then((res) => res.data)
+    .catch((err) => err);
+}
+
+function removeCartItem(productId) {
+  return axios
+    .delete(`https://ecommerce.routemisr.com/api/v1/cart/${productId}`, {
+      headers,
+    })
+    .then((res) => res.data)
+    .catch((err) => err);
+}
+
+function updateCartProductQuantity(productId, count) {
+  return axios
+    .put(
+      `https://ecommerce.routemisr.com/api/v1/cart/${productId}`,
+      { count },
+      { headers }
+    )
+    .then((res) => res.data)
+    .catch((err) => err);
+}
+
+function clearUserCart() {
+  return axios
+    .delete("https://ecommerce.routemisr.com/api/v1/cart", { headers })
+    .then((res) => res.data)
+    .catch((err) => err);
+}
+
 export default function CartContextProvider({ children }) {
   return (
-    <CartContext.Provider value={{ addToCart }}>
+    <CartContext.Provider
+      value={{
+        addToCart,
+        getLoggedUserCart,
+        removeCartItem,
+        updateCartProductQuantity,
+        clearUserCart,
+      }}
+    >
       {children}
     </CartContext.Provider>
   );
