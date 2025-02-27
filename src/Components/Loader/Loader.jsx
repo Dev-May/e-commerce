2593fbc346e10e9d7211caca3dd0e72cfa8797cd
Dev-May <mai.eldhrawy@gmail.com>
2025-02-27 17:22:16 +0200

    ui updates

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Loader() {
   return (
-    <div role="status">
+    <div role="status" className="mx-auto">
       <svg
         aria-hidden="true"
         className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-[#0aad0a]"
