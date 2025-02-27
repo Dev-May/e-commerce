3af7e83fd176eacd21f600074662c07e11ce09ed
Dev-May <mai.eldhrawy@gmail.com>
2025-02-27 17:21:37 +0200

    brands component

--- src/Pages/Brands/Brands.jsx
+++ src/Pages/Brands/Brands.jsx
@@ -1,5 +1,31 @@
+import axios from "axios";
 import styles from "./Brands.module.css";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Brands() {
-  return <div>Brands</div>;
+  function getBrands() {
+    return axios.get("https://ecommerce.routemisr.com/api/v1/brands");
+  }
+
+  const { data, isLoading, isFetching } = useQuery({
+    queryKey: ["brands"],
+    queryFn: getBrands,
+  });
+
+  console.log(data?.data?.data);
+  console.log("isLoading: ", isLoading);
+  console.log("isFetching: ", isFetching);
+
+  return (
+    <div className="row my-10">
+      {data?.data.data?.map((brand, i) => (
+        <div key={i} className="w-1/4 p-4">
+          <div className="inner">
+            <img src={brand.image} alt={brand.name} className="w-full" />
+            {/* <h4 className="text-center font-semibold">{brand.name}</h4> */}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
